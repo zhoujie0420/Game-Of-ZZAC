145d3465bf0b2f303c637e55131f619db5c4b72b
zhoujie <1747364257@qq.com>
2022-10-11 22:12:56 +0800

    注册登录+持久化+前端页面授权

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,41 +7,11 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
-import $ from "jquery"
+
 export default {
   components: {
     NavBar
   },
-  setup(){
-    $.ajax({
-      url:"http://127.0.0.1:3000/user/account/token/",
-      type :"post",
-      data:{
-        username : 'z',
-        password : 'z',
-      },
-      success(resp){
-        console.log(resp);
-      },
-      error(resp){
-        console.log(resp);
-      }
-    });
-    
-   $.ajax({
-    url:"http://127.0.0.1:3000/user/account/info/",
-    type:"get",
-    headers :{
-      Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMGIyOWRkZmVhOTY0MzhkYjg3NDQ2NzFkYWY3ZTg0OSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY2NTQ2MzA5OCwiZXhwIjoxNjY2NjcyNjk4fQ.6So-Of3krMqCla04ThEA0wou8XqDraod0gH-L9xSVp0",
-    },
-    success(resp){
-      console.log(resp);
-    },
-    error(resp){
-      console.log(resp);
-    }
-   })
-  }
   
 }
 </script>
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import ModuleUser from './user'
+
 
 export default createStore({
   state: {
@@ -10,5 +12,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    user: ModuleUser,
   }
 })
