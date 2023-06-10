0d801408a79a4b2db414a92d860be3594375d06a
wangmei  <1747364257@qq.com>
2023-06-10 13:20:43 +0800

    nacos 集成

--- backendcloud/backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backendcloud/backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.kob.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BackendApplication {
 
     public static void main(String[] args) {
--- backendcloud/botrunningsystem/src/main/java/com/kob/botrunningsystem/BotRunningSystemApplication.java
+++ backendcloud/botrunningsystem/src/main/java/com/kob/botrunningsystem/BotRunningSystemApplication.java
@@ -3,8 +3,10 @@ package com.kob.botrunningsystem;
 import com.kob.botrunningsystem.service.impl.BotRunningServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BotRunningSystemApplication {
     public static void main(String[] args) {
         BotRunningServiceImpl.botPool.start();
