3d4c512c844756a5bcaac7d4443be7cebfa2a7dd
jiezhou <1747364257@qq.com>
2024-03-07 18:04:40 +0800

    test

--- web/src/views/SigningRecordView.vue
+++ web/src/views/SigningRecordView.vue
@@ -3,120 +3,117 @@
     <table class="table table-striped table-hover" style="text-align: center;">
       <thead>
       <tr>
-        <th>玩家</th>
-        <th>天梯分</th>
-        <th>胜率</th>
+        <th>{{ $store.state.user.role === 1 ? '医生姓名' : '患者姓名' }}</th>
+        <th>更新时间</th>
+        <th>签约文档</th>
+        <th>状态</th>
+        <th>操作</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="user in users" :key="user.id">
-        <td>
-          <img :src="user.photo" alt="" class="record-user-photo">
-          &nbsp;
-          <span class="record-user-username">{{ user.username }}</span>
+      <tr v-for="record in signRecords" :key="record.id">
+        <td>{{ record.otherName }}</td>
+        <td>{{ record.createTime }}</td>
+        <td @click="redirectToUrl(record.docUrl)">
+          {{ record.docUrl === null ? '待上传' : '点击查看' }}
         </td>
-        <td>{{ user.rating }}</td>
+        <td>{{ record.status === 1 ? '已完成' : '未完成' }}</td>
+        <td>
+          <div class="custom-file">
+            <label :for="'customFile_' + record.id" class="custom-file-upload">
+              <input type="file" @change="upload(record.id,record.doctorId, $event)" :id="'customFile_' + record.id" style="display: none;">
+              上传文件
+            </label>
+          </div>
+        </td>
       </tr>
       </tbody>
     </table>
-    <nav aria-label="...">
-      <ul class="pagination" style="float: right;">
-        <li class="page-item" @click="click_page(-2)">
-          <a class="page-link" href="#">前一页</a>
-        </li>
-        <li :class="'page-item ' + page.is_active" v-for="page in pages" :key="page.number" @click="click_page(page.number)">
-          <a class="page-link" href="#">{{ page.number }}</a>
-        </li>
-        <li class="page-item" @click="click_page(-1)">
-          <a class="page-link" href="#">后一页</a>
-        </li>
-      </ul>
-    </nav>
-
   </ContentField>
 </template>
 
-<script>
-import ContentField from '../components/ContentField.vue'
-import { useStore } from 'vuex';
-import { ref } from 'vue';
+<script setup>
+import ContentField from '../components/ContentField.vue';
+import {ref} from "vue";
+import store from "@/store";
+
+const signRecords = ref([]); // 创建响应式的userInfos
 import $ from 'jquery';
 
-export default {
-  components: {
-    ContentField
-  },
-  setup() {
-    const store = useStore();
-    let users = ref([]);
-    let current_page = 1;
-    let total_users = 0;
-    let pages = ref([]);
+getSigns()
 
-    const click_page = page => {
-      if (page === -2) page = current_page - 1;
-      else if (page === -1) page = current_page + 1;
+function upload(id,doctorId,e) {
+  let file = e.target.files[0];
+  let param = new FormData();
+  param.append("file", file);
+  $.ajax({
+    url: "http://127.0.0.1:3000/api/aliyun/oss/upload",
+    type: "post",
+    data: param,
+    processData: false,
+    contentType: false,
+    headers: {
+      "Authorization": "Bearer " + store.state.user.token,
+    },
+    success(resp) {
+      console.log("record =====" + id);
+      console.log(resp);
+      updateDocUrl(id,doctorId, resp);
+    }
+  })
+}
 
-      let max_pages = parseInt(Math.ceil(total_users / 10));
-
-      if (page >= 1 && page <= max_pages) {
-        pull_page(page);
+function updateDocUrl(id , doctorId,url) {
+  $.ajax({
+    url: `http://127.0.0.1:3000/api/signing/updateSigning/`,
+    type: "post",
+    data:
+        {
+          "id": id,
+          "docUrl": url,
+          "doctorId": doctorId,
+        },
+    headers: {
+      Authorization: "Bearer " + store.state.user.token,
+    },
+    success(resp) {
+      if (resp.code === 200) {
+        getSigns()
       }
-    }
+    },
+  });
+}
 
-    const udpate_pages = () => {
-      let max_pages = parseInt(Math.ceil(total_users / 10));
-      let new_pages = [];
-      for (let i = current_page - 2; i <= current_page + 2; i ++ ) {
-        if (i >= 1 && i <= max_pages) {
-          new_pages.push({
-            number: i,
-            is_active: i === current_page ? "active" : "",
-          });
-        }
-      }
-      pages.value = new_pages;
-    }
-
-    const pull_page = page => {
-      current_page = page;
-      $.ajax({
-        url: "http://127.0.0.1:3000/api/ranklist/getlist/",
-        data: {
-          page,
-        },
-        type: "get",
-        headers: {
-          Authorization: "Bearer " + store.state.user.token,
-        },
-        success(resp) {
-          users.value = resp.users;
-          console.log(users.value);
-          total_users = resp.users_count;
-          udpate_pages();
-        },
-        error(resp) {
-          console.log(resp);
-        }
-      })
-    }
-
-    pull_page(current_page);
-
-    return {
-      users,
-      pages,
-      click_page
-    }
-
+function redirectToUrl(url) {
+  if (url !== null) {
+    window.open(url, '_blank');
   }
+}
+function getSigns() {
+  $.ajax({
+    url: `http://127.0.0.1:3000/api/signing/getSigning/`,
+    type: "post",
+    headers: {
+      Authorization: "Bearer " + store.state.user.token,
+    },
+    success(resp) {
+      if (resp.code === 200) {
+        console.log(resp.data);
+        signRecords.value = resp.data; // 更新userInfos的值
+      } else {
+        console.log(resp.message);
+      }
+    },
+  });
 }
 </script>
 
 <style scoped>
-img.record-user-photo {
-  width: 4vh;
-  border-radius: 50%;
+.custom-file-upload {
+  display: inline-block;
+  padding: 2px 15px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
 }
-
 </style>
